ab50ee4080235e0d4b9004acd1b23a167994de4d
Brosigx <ambrosio.guardiola@proton.me>
2025-06-24 20:13:46 +0200

    Vista la separacion de la logica

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+import './Button.css'
+
+
+/*
+RENDER: ¿Qué es el render? Básicamente es ejecutar una función. Cada vez que ejecutamos algo, por ejemplo, la function App(),
+estariamos haciendo un render. Si en nuestra aplicación tenemos un HTML, y este tiene dentro un dashboard, el dashboard esta
+ejecutando una función, y el resultado de ejecutar esa función, se vuelve a meter donde estará el dashboard, creando un renderizado.
+IMPORTANTE: Nuestra función como programadores es que haya la mínima cantidad de renders, porque esto consume recursos.
+*/
+
+/*
+COMPONENTE: Es muy importante que siempre que hagamos un componente, sea la minima unidad logica posible.
+En este apartado vamos a crear un componente dentro de esta funcion App, y lo que creemos ahi dentro, se va a ir al HTML. (Al DOM)
+*/
+
+
+/*
+  Por supuesto tambien podemos agregar caracteristicas a style desde fuera. Seria lo mismo que hacerlo desde dentro
+  de la funcion. Tambien podemos hacerlo crecer, de esta forma no tenemos que ir creando las cosas dentro de la funcion.
+  Lo ponemos fuera de la funcion ya que va a ser reutilizable.
+  */
+
+
+  /*
+  Si fueramos unos programadores puta madre, en vez de declarar esto fuera de la funcion, lo que hariamos seria crear el button
+  fuera, en un fichero .css y luego importar la parte que necesitemos dentro de nuestro .tsx. Vamos a hacerlo de esta forma.
+
+  const style = {
+  button: {
+    color: "Blue",
+    backgroundColor: "grey",
+  }
+
+}
+  */
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+
+
+
   return (
+
+    /*Vamos a añadir un estilo al boton onClick. Hay una diferenciacion de cuando hablamos HTML a React, por ejemplo, para poner clase
+    no es Class = "name", si no que seria ClassName={}. La clave es ver que esto va dentro de llavecitas, lo que significa que va
+    a haber logica dentro de ese componente, lo que hara que este fichero sea .tsx en vez de un .html normal 
+    */
+
+
+    /*
+    Quitamos el color y background color de dentro del style, ya que lo hemos declarado de forma global desde fuera,
+    y asi puede ser reutilizable.
+    */
+
+    //Quitamos todo el estilo del boton, porque lo correcto es crear un componente Button
     <>
-      <div>
-        <a href="si o no marino" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      
+      <button className="Boton personalizado" /*style={color: "Blue", backgroundColor:"green" style.button}*/onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
       </p>
+
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,66 @@
+
+/*
+Aqui si te fijas, solo estamos importando el strictMode, no nos interesa importar todo react, porque se estarian importando
+muchas cosas. por ejemplo, podriamos estar haciendo 
+
+Hacer esto es una guarreria de manual, es mucho mejor hacerlo con llaves.
+---> <React.StrictMode>
+  </>
+  */
 import { StrictMode } from 'react'
+
+//Dom, si te acuerdas de Humberto, se encarga de la parte de la renderizacion del navegador
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+//Aqui estamos importando todo el contenido de app.tsx, porque no lo estamos poniendo entre llaves.
 import App from './App.tsx'
 
+//El createRoot tiene una exclamacion porque puede ser que el ID no exista
 createRoot(document.getElementById('root')!).render(
+
+  /*
+  El strcit mode se encarga de controlar la creacion de componentes, rompiendolo y creandolo de nuevo
+  de esta forma se ve si mantiene el mismo estado que antes de crearlo, para controlar el correcto funcionamiento. En
+  produccion se quita.
+  */
+  
+  /*
+  Si te fijas, estamos importando la funcion App, declarada en el fichero App.tsx. JSX es la capacidad de tener HTML sumado a que
+  tiene capacdiad de logica dentro del HTML. Es tan facil como fijarte en la extension de los ficheros. Ahora mismo podemos ver que
+  tenemos tsx. Si quitasemos la x, simplemente seria HTML sin logica ninguna.
+
+  DUDA: Siempre tenemos que poner extension jsx o tsx cuando creamos un fichero que va a contener JavaScript? NO!!!!
+  
+  */
   <StrictMode>  
     <App />
   </StrictMode>,
+
+  /*
+  1- Esto es una SPA, SINGLE PAGE APPLICATION, es decir, se renderiza todo lo que hay ahi dentro en una sola pagina.
+  La magia de esto es que haya deteccion de cambios para poder ver los cambios conforme los vamos creando.
+  La deteccion de cambios se hace mediante los triggers, es decir, eventos que despierten un proceso de render, por ejemplo,
+  el acceder a un endpoint, una llamada a una api, pulsar un boton... muchas cosas.
+
+  2 - Tenemos un trigger inicial, que se encarga de montar todo el componente. Y luego tendremos un segundo trigger, que se
+  encarga del re-render. ¿Que puede hacer que algo se re-renderice? Un botón... muchas cosas.
+
+  3 - Tenemos dos tipos de DOM, un DOM normal, y un DOM virtual, el DOM normal contiene lo que ya esta montado, mientras que el 
+  DOM virtual contiene una copia del DOM original pero con todos los cambios creados hasta el momento. El render lo que hace comparar
+  ambos DOM.
+
+  4 - ¿Qué es un commit en REACT? Es el proceso de comparar ambos DOM, como se ha explicado antes.
+
+  EJEMPLO: En el contexto de un restaurante, tenemos un trigger que seria cuando un cliente hace un pedido, el proceso
+  de renderizado seria CREAR el componente, en este caso, crear el plato. Luego tendriamos que entregar el plato que se
+  ha pedido, sería el proceso del commit.
+  */
+
+   
 )
 
-//LOS CABRONES DE META NO QUIEREN QUE USES MAS EL CREATE-REACT, ESTA DEPRECATED
+/*
+Meta no quiere que uses create-react-app porque ahora puedes usar Vite y Next.js que ofrece mucho
+mejor rendimiento.
+*/
